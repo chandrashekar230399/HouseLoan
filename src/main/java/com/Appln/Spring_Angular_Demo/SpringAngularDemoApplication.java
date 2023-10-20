728e69016e11539c8bc4a6cb0b198a8ae7a664b9
sht-jaga <jagadeesh7marolix@gmail.com>
2023-10-20 08:51:47 +0530

    first commit

---
+++ src/main/java/com/Appln/Spring_Angular_Demo/SpringAngularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Appln.Spring_Angular_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Appln/Spring_Angular_Demo/SpringAngularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Appln.Spring_Angular_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
